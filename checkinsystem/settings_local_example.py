bfd72960e5467f5af7906f7721513021d194fbd5
xiaohuanshu <xiaohuanshu@gmail.com>
2016-03-17 20:07:28 +0800

    change local setting handle

---
+++ checkinsystem/settings_local_example.py
@@ -0,0 +1,43 @@
+# Develop Environment Settings
+try:
+    from settings_base import *
+except:
+    pass
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+DOMAIN = ''
+
+# Super admin password
+SAFENUMBER = ''
+
+# Wechatmanage.py
+TOKEN = ''
+APPID = ''
+APPSECRET = ''
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432'
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '',
+    }
+}
+
+INTERNAL_IPS = ("127.0.0.1")
--- checkinsystem/wsgi.py
+++ checkinsystem/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "checkin.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "checkinsystem.settings")
 
 application = get_wsgi_application()

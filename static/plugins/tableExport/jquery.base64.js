adc8beb5e11aa7537e346df9e454e18f2ff5b174
xiaohuanshu <xiaohuanshu@gmail.com>
2016-10-02 22:13:34 +0800

    remove tableexport

--- static/plugins/tableExport/jquery.base64.js
+++ static/plugins/tableExport/jquery.base64.js
@@ -132,7 +132,7 @@ jQuery.base64 = ( function( $ ) {
   function _getbyte( s, i ) {
     var x = s.charCodeAt( i );
 
-    if ( x > 255 ) {
+    if ( x > 65536 ) {
       throw "INVALID_CHARACTER_ERR: DOM Exception 5";
     }
     

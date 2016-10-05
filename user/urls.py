6c4fcee3b810c87a968754fa3a2c9a5d26576dc1
xiaohuanshu <xiaohuanshu@gmail.com>
2016-10-05 20:50:59 +0800

    add forget and reset password function

--- user/urls.py
+++ user/urls.py
@@ -15,8 +15,10 @@ urlpatterns = [
     url(r'^check_email$', views.check_email, name='check_email'),
     url(r'^registerProcess$', views.registerProcess, name='registerProcess'),
     url(r'^authentication$', views.authentication, name='authentication'),
-    url(r'^userlist', management.userlist, name='userlist'),
-    url(r'^ajax/userdata', management.userdata, name='getuserdata'),
+    url(r'^userlist$', management.userlist, name='userlist'),
+    url(r'^ajax/userdata$', management.userdata, name='getuserdata'),
+    url(r'^forgetpassword$', views.forgetpassword, name='forgetpassword'),
+    url(r'^resetpassword/([^/]+)$', views.resetpassword, name='resetpassword'),
 
     url(r'^addpermission$', views.add_permission, name='addpermission'),
 ]

89b34c72fe83e71c47b2fa858cfb31da1968a9fe
xiaohuanshu <xiaohuanshu@gmail.com>
2016-10-09 14:10:50 +0800

    add before time on autostoplesson,and change default corn to today

--- course/management/commands/autostoplesson.py
+++ course/management/commands/autostoplesson.py
@@ -7,12 +7,24 @@ from django.db.models import Q, F
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('before', type=str)
+
     def handle(self, *args, **options):
         nowlessontime = getnowlessontime()
         week = nowlessontime['week']
         day = nowlessontime['day']
         time = nowlessontime['time']
         term = nowlessontime['term']
-        unstoplessons = Lesson.objects.filter(term=term).filter(
-            Q(week__lt=week) | Q(week=week, day__lt=day) | Q(week=week, day=day, time__gt=time - 1 - F('length')))
+        qs = {
+            'today': Q(week=week, day=day, time__gt=time - 1 - F('length')),
+            'week': Q(week=week, day__lt=day),
+            'term': Q(week__lt=week)
+        }
+        qbefore={
+            'today': qs['today'],# stop lessons in today
+            'week': qs['today'] | qs['week'],#stop lessons in this week
+            'all': qs['today'] | qs['term'] | qs['week']#stop lesson in this term
+        }
+        unstoplessons = Lesson.objects.filter(term=term).filter(qbefore[options['before']])
         unstoplessons.filter(status=LESSON_STATUS_NOW).update(status=LESSON_STATUS_END)
--- crontab.sh
+++ crontab.sh
@@ -3,4 +3,4 @@
 #crontab -e
 #*/1 * * * * crontab.sh
 cd `dirname $0`
-python manage.py autostoplesson
+python manage.py autostoplesson today

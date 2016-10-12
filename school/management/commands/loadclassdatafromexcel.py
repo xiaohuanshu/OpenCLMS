14e29657f675da838f80a0bb514328d68b0b64f1
xiaohuanshu <xiaohuanshu@gmail.com>
2016-10-12 21:25:23 +0800

    add loadclassdatafromexcel function

---
+++ school/management/commands/loadclassdatafromexcel.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand, CommandError
+from school.models import Class, Department, Major
+import xlrd
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('excelfile', type=int)
+
+    def handle(self, *args, **options):
+        rb = xlrd.open_workbook(options['excelfile'])
+        rs = rb.sheets()[0]
+        count = 0
+        for i in range(rs.nrows):
+            try:
+                count += 1
+                name = rs.cell(i, 0).value
+                data, created = Class.objects.get_or_create(name=name)
+                data.department = Department.objects.get(name=rs.cell(i, 2).value)
+                data.major = Major.objects.get(name=rs.cell(i, 1).value)
+                data.schoolyear = rs.cell(i, 3).value
+                data.save()
+            except:
+                print "error on class:%s" % name
+                count -= 1
+        print "successful upgrade %d" % count

139165d42b0000fa51809237331f939d994b6e96
xiaohuanshu <xiaohuanshu@gmail.com>
2016-10-13 10:25:35 +0800

    add loadteacherdatafromexcel function

---
+++ school/management/commands/loadteacherdatafromexcel.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand, CommandError
+from school.models import Teacher, Department, Administration, Teachertoadministration, Teachertodepartment
+import xlrd
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('excelfile', type=str)
+
+    def handle(self, *args, **options):
+        rb = xlrd.open_workbook(options['excelfile'])
+        rs = rb.sheets()[0]
+        count = 0
+        for i in range(rs.nrows):
+            try:
+                count += 1
+                teacherid = rs.cell(i, 0).value
+                data, created = Teacher.objects.get_or_create(teacherid=teacherid)
+                data.name = rs.cell(i, 1).value
+                data.idnumber = rs.cell(i, 2).value
+                if rs.cell(i, 3).value == u"男":
+                    sex = 1
+                else:
+                    sex = 2
+                data.sex = sex
+
+                department = Department.objects.get(name=rs.cell(i, 3).value)
+                Teachertodepartment.objects.filter(teacher=data).delete()
+                Teachertodepartment(teacher=Teacher, administration=administration).save()
+                administration = Administration.objects.get(name=rs.cell(i, 4).value)
+                Teachertoadministration.objects.filter(teacher=data).delete()
+                Teachertoadministration(teacher=Teacher, administration=administration).save()
+                data.save()
+            except:
+                print "error on teacherid:%s" % teacherid
+                count -= 1
+        print "successful upgrade %d" % count

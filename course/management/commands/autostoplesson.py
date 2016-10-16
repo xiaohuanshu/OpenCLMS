51f97d6b639889b401be9d6c75cd394252c95d03
xiaohuanshu <xiaohuanshu@gmail.com>
2016-10-16 21:45:07 +0800

    fix autoclose bug

--- course/management/commands/autostoplesson.py
+++ course/management/commands/autostoplesson.py
@@ -17,14 +17,15 @@ class Command(BaseCommand):
         time = nowlessontime['time']
         term = nowlessontime['term']
         qs = {
-            'today': Q(week=week, day=day, time__lt=time - 1 - F('length')),
+            'today': Q(week=week, day=day, time__lt=time + 1 - F('length')),
+            # 上课时间 + 课程长度 - 1 < 现在时间   =>  上课时间 < 现在时间 + 1 - 课程长度
             'week': Q(week=week, day__lt=day),
             'term': Q(week__lt=week)
         }
-        qbefore={
-            'today': qs['today'],# stop lessons in today
-            'week': qs['today'] | qs['week'],#stop lessons in this week
-            'all': qs['today'] | qs['term'] | qs['week']#stop lesson in this term
+        qbefore = {
+            'today': qs['today'],  # stop lessons in today
+            'week': qs['today'] | qs['week'],  # stop lessons in this week
+            'all': qs['today'] | qs['term'] | qs['week']  # stop lesson in this term
         }
         unstoplessons = Lesson.objects.filter(term=term).filter(qbefore[options['before']])
         unstoplessons.filter(status=LESSON_STATUS_NOW).update(status=LESSON_STATUS_END)

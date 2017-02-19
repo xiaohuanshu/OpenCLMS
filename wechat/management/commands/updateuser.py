b4f8bf3199175070c6738c4ee60e4c2ba9764041
xiaohuanshu <xiaohuanshu@gmail.com>
2017-02-19 16:04:34 +0800

    fix:更新微信通讯录添加身份证号

--- wechat/management/commands/updateuser.py
+++ wechat/management/commands/updateuser.py
@@ -2,7 +2,7 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from wechat.client import wechat_client
-from school.models import Student,Teacher
+from school.models import Student, Teacher
 import json
 
 
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
         def getinfomation(id):
             for user in userlist:
-                #print json.dumps(user, encoding='utf-8', ensure_ascii=False)
+                # print json.dumps(user, encoding='utf-8', ensure_ascii=False)
                 if user['extattr']['attrs'][0]['name'] == u"学工号":
                     thisuserworkid = user['extattr']['attrs'][0]['value']
                 else:
@@ -21,7 +21,13 @@ class Command(BaseCommand):
                     return user
             return None
 
-        #print json.dumps(userlist, encoding='utf-8', ensure_ascii=False)
+        def getidnumber(user):
+            if user['extattr']['attrs'][0]['name'] == u"身份证号":
+                return user['extattr']['attrs'][0]['value']
+            else:
+                return user['extattr']['attrs'][1]['value']
+
+        # print json.dumps(userlist, encoding='utf-8', ensure_ascii=False)
 
         # for student
         students = Student.objects.select_related('classid').all()
@@ -30,16 +36,16 @@ class Command(BaseCommand):
             userinfo = getinfomation(s.studentid)
             if userinfo:
                 print 'exist'
-                if userinfo['department'][0] != s.classid.wechatdepartmentid:
-                    wechat_client.user.update(userinfo['userid'], department=[s.classid.wechatdepartmentid])
+                if userinfo['department'][0] != s.classid.wechatdepartmentid or getidnumber(userinfo)[
+                                                                                -6:] != s.idnumber:
+                    wechat_client.user.update(userinfo['userid'], department=[s.classid.wechatdepartmentid], extattr={
+                        "attrs": [{"name": u"学工号", "value": s.studentid}, {"name": u"身份证号", "value": s.idnumber[-6:]}]})
             else:
                 print 'create'
-                wechat_client.user.create('S%s' % s.studentid, s.name, s.classid.wechatdepartmentid, u'学生', gender=s.sex,
+                wechat_client.user.create('S%s' % s.studentid, s.name, s.classid.wechatdepartmentid, u'学生',
+                                          gender=s.sex,
                                           email='%s@gengdan.edu.cn' % s.studentid, extattr={
                         "attrs": [{"name": u"学工号", "value": s.studentid}, {"name": u"身份证号", "value": s.idnumber[-6:]}]})
-
-
-
 
         # for teacher
         teachers = Teacher.objects.all()
@@ -50,8 +56,10 @@ class Command(BaseCommand):
 
             if userinfo:
                 print 'exist'
-                if sorted(userinfo['department']) != sorted(tdeps):
-                    wechat_client.user.update(userinfo['userid'], department=tdeps)
+                if sorted(userinfo['department']) != sorted(tdeps) or getidnumber(userinfo)[
+                                                                      -6:] != t.idnumber:
+                    wechat_client.user.update(userinfo['userid'], department=tdeps, extattr={
+                        "attrs": [{"name": u"学工号", "value": t.teacherid}, {"name": u"身份证号", "value": t.idnumber[-6:]}]})
             else:
                 print 'create'
                 wechat_client.user.create('T%s' % t.teacherid, t.name, tdeps, u'教师',

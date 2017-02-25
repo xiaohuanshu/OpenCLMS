3cd5e6d21ffae45c576dbf729aa252d6229854ad
xiaohuanshu <xiaohuanshu@gmail.com>
2017-02-25 20:37:56 +0800

    increase course time's strlengh

--- course/models.py
+++ course/models.py
@@ -23,7 +23,7 @@ class Course(models.Model):
     number = models.SmallIntegerField(blank=True, null=True)
     title = models.CharField(max_length=100, blank=True, null=True)
     teacher = models.ForeignKey('school.Teacher', models.DO_NOTHING, db_column='teacherid', blank=True, null=True)
-    time = models.CharField(max_length=255, blank=True, null=True)
+    time = models.CharField(max_length=400, blank=True, null=True)
     location = models.CharField(max_length=255, blank=True, null=True)
     schoolterm = models.CharField(max_length=20, blank=True, null=True)
     major = models.ForeignKey('school.Major', models.DO_NOTHING, db_column='majorid', blank=True, null=True)

e0e4c9f481660774430ad85ee0ca398765106736
xiaohuanshu <xiaohuanshu@gmail.com>
2017-07-23 22:43:49 +0800

    fix bug

--- course/context_processors.py
+++ course/context_processors.py
@@ -5,7 +5,7 @@ from school.function import getnowlessontime
 def todaylesson(request):
     if hasattr(request, 'user'):
         nowlessontime = getnowlessontime()
-        if request.user.isteacher():
+        if request.user.isteacher:
             data = Lesson.objects.filter(course__in=request.user.teacher_set.get().course_set.all(), term=nowlessontime['term'],
                                          day=nowlessontime['day'], week=nowlessontime['week']).select_related(
                 'course').all()

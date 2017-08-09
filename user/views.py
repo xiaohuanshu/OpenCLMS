15e8e26d1f0e8275cb6d9c6b9a05d8423140e4d8
xiaohuanshu <xiaohuanshu@gmail.com>
2017-08-09 16:53:05 +0800

    整合用户列表功能到教师、学生列表中

--- user/views.py
+++ user/views.py
@@ -293,37 +293,3 @@ def resetpassword(request, uuidstr):
                                                     'jumpurl': reverse('user:login', args=[])})
         else:
             return render(request, 'resetpassword.html', {})
-
-
-@permission_required(permission='user_viewlist')
-def userlist(request):
-    return render(request, 'user.html')
-
-
-@permission_required(permission='user_viewlist')
-def userdata(request):
-    limit = int(request.GET['limit'])
-    offset = int(request.GET['offset'])
-    userdata = User.objects.order_by('id')
-    search = request.GET.get('search', '')
-    if not search == '':
-        count = userdata.filter(username__icontains=search).count()
-        userdata = userdata.filter(username__icontains=search)[offset: (offset + limit)]
-    else:
-        count = userdata.count()
-        userdata = userdata.all()[offset: (offset + limit)]
-
-    rows = []
-    for p in userdata:
-        ld = {
-            'id': p.id,
-            'username': p.username,
-            'sex': (p.sex - 1 and [u'女'] or [u'男'])[0] if p.sex else '',
-            'ip': p.ip,
-            'iswechat': (p.openid and [u'是'] or [u'否'])[0],
-            'lastlogintime': p.lastlogintime.strftime('%Y-%m-%d %H:%M:%S') if p.lastlogintime else '',
-            'role': ", ".join(role.name for role in p.role.all())
-        }
-        rows.append(ld)
-    data = {'total': count, 'rows': rows}
-    return HttpResponse(json.dumps(data), content_type="application/json")

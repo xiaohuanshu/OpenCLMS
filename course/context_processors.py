830e4d6b64b15e540d0601fbac819a3eb4ce7710
xiaohuanshu <xiaohuanshu@gmail.com>
2017-09-11 10:54:29 +0800

    优化效率

--- course/context_processors.py
+++ course/context_processors.py
@@ -3,6 +3,8 @@ from school.function import getnowlessontime
 
 
 def todaylesson(request):
+    if request.is_ajax():
+        return {}
     if hasattr(request, 'user'):
         nowlessontime = getnowlessontime()
         if request.user.isteacher:

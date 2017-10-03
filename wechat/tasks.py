0095d000a2fdba1261dd8cc6480ce660032f774e
xiaohuanshu <xiaohuanshu@gmail.com>
2017-10-03 20:40:36 +0800

    添加每日生日祝福功能（未启用）

--- wechat/tasks.py
+++ wechat/tasks.py
@@ -1,12 +1,15 @@
+# -*- coding: utf-8 -*-
 # Create your tasks here
 from celery import shared_task
-
+from django.conf import settings
 from wechat.client import wechat_client
 from user_system.models import User
+from school.models import Student
 from django.core.files import File
 from django.core.files.temp import NamedTemporaryFile
 import urllib2
 import logging
+import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -28,3 +31,15 @@ def update_avatar_from_wechat():
     return counter
 
 
+@shared_task(name='send_birthday_blessing')
+def send_birthday_blessing():
+    counter = 0
+    todate = datetime.datetime.now().strftime('%m%d')
+    students = Student.objects.select_related('user').exclude(user__openid=None) \
+        .extra(where=['substring("idnumber",11,4) = \'%s\'' % (todate)]).all()
+    for s in students:
+        counter += 1
+        message = u"%s同学，生日快乐哦！祝你有美好的一天~" % s.name
+        wechat_client.message.send_text(agent_id=settings.AGENTID, user_ids=[s.user.openid], content=message)
+    logger.info("[send_birthday_blessing]successful send %d students" % counter)
+    return counter

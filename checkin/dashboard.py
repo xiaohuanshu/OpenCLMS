f6c5310cfe53454f403bbacd68e9ac0e818e4726
xiaohuanshu <xiaohuanshu@gmail.com>
2018-01-06 23:22:56 +0800

    每周总览

--- checkin/dashboard.py
+++ checkin/dashboard.py
@@ -23,6 +23,10 @@ def today(request):
     return render(request, 'dashboard_today.html', {})
 
 
+def week(request):
+    return render(request, 'dashboard_week.html', {})
+
+
 @cache_page(60)
 def today_data(request):
     data = {}
@@ -182,6 +186,155 @@ def today_data(request):
                  'coursetime_early_value': coursetime_early_value,
                  'coursetime_lateearly_value': coursetime_lateearly_value,
                  'coursetime_arrive_value': coursetime_arrive_value})
+
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+@cache_page(60)
+def week_data(request):
+    data = {}
+    now_lesson_time = getnowlessontime()
+    now_week = now_lesson_time['week']
+    now_term = now_lesson_time['term']
+
+    week_lessons = Lesson.objects.filter(week=now_week, term=now_term)
+    data['week_lessons'] = week_lessons.count()
+    week_start_lessons = week_lessons.filter(status__in=[LESSON_STATUS_NOW, LESSON_STATUS_CHECKIN,
+                                                         LESSON_STATUS_CHECKIN_ADD, LESSON_STATUS_CHECKIN_AGAIN,
+                                                         LESSON_STATUS_END])
+    data['week_start_lessons'] = week_start_lessons.count()
+    week_start_checkin_lessons = week_start_lessons.filter(checkincount__gt=0)
+    data['week_start_checkin_lessons'] = week_start_checkin_lessons.count()
+    # course_status
+    course_status_data = Checkin.objects.filter(lesson__in=week_start_lessons).aggregate(
+        normal=Count(Case(When(status=CHECKIN_STATUS_NORMAL, then=1))),
+        private_ask=Count(Case(When(status=CHECKIN_STATUS_PRIVATE_ASK, then=1))),
+        success=Count(Case(When(status=CHECKIN_STATUS_SUCCESS, then=1))),
+        early=Count(Case(When(status=CHECKIN_STATUS_EARLY, then=1))),
+        late=Count(Case(When(status=CHECKIN_STATUS_LATE, then=1))),
+        lateearly=Count(Case(When(status=CHECKIN_STATUS_LATEEARLY, then=1))),
+        public_ask=Count(Case(When(status=CHECKIN_STATUS_PUBLIC_ASK, then=1))),
+        sick_ask=Count(Case(When(status=CHECKIN_STATUS_SICK_ASK, then=1))),
+    )
+    course_status_data['arrive'] = course_status_data['success'] + course_status_data['early'] + \
+                                   course_status_data['late'] + course_status_data['lateearly']
+    course_status_data['ask'] = course_status_data['private_ask'] + course_status_data['public_ask'] + \
+                                course_status_data['sick_ask']
+
+    data['course_status_data'] = course_status_data
+    # student_status
+    student_all_count = Checkin.objects.filter(lesson__in=week_start_lessons).distinct('student').count()
+    student_all_arrive = Checkin.objects.filter(lesson__in=week_start_lessons). \
+        annotate(all=Count('status'), num=Count(Case(When(status=CHECKIN_STATUS_SUCCESS, then=1)))).filter(num=F('all'))
+    student_all_arrive.query.group_by = 'student'
+    student_all_arrive = student_all_arrive.count()
+    student_all_normal = Checkin.objects.filter(lesson__in=week_start_lessons). \
+        annotate(all=Count('status'), num=Count(Case(When(status=CHECKIN_STATUS_NORMAL, then=1)))).filter(num=F('all'))
+    student_all_normal.query.group_by = 'student'
+    student_all_normal = student_all_normal.count()
+    student_public_ask = Checkin.objects.filter(lesson__in=week_start_lessons).filter(
+        status=CHECKIN_STATUS_PUBLIC_ASK).distinct('student').count()
+    student_private_ask = Checkin.objects.filter(lesson__in=week_start_lessons).filter(
+        status=CHECKIN_STATUS_PRIVATE_ASK).distinct('student').count()
+    student_sick_ask = Checkin.objects.filter(lesson__in=week_start_lessons).filter(
+        status=CHECKIN_STATUS_SICK_ASK).distinct('student').count()
+    student_other = Checkin.objects.filter(lesson__in=week_start_lessons).exclude(
+        status__in=[CHECKIN_STATUS_SUCCESS, CHECKIN_STATUS_NORMAL, CHECKIN_STATUS_PUBLIC_ASK,
+                    CHECKIN_STATUS_PRIVATE_ASK, CHECKIN_STATUS_SICK_ASK]).distinct('student').count()
+    student_late_count = Checkin.objects.filter(lesson__in=week_start_lessons).filter(
+        Q(status=CHECKIN_STATUS_LATE) | Q(status=CHECKIN_STATUS_LATEEARLY)).distinct('student').count()
+    student_early_count = Checkin.objects.filter(lesson__in=week_start_lessons).filter(
+        Q(status=CHECKIN_STATUS_EARLY) | Q(status=CHECKIN_STATUS_LATEEARLY)).distinct('student').count()
+    student_normal_count = Checkin.objects.filter(lesson__in=week_start_lessons).filter(
+        status=CHECKIN_STATUS_NORMAL).distinct('student').count()
+    course_late_count = Checkin.objects.filter(lesson__in=week_start_lessons).filter(
+        Q(status=CHECKIN_STATUS_LATE) | Q(status=CHECKIN_STATUS_LATEEARLY)).count()
+    course_early_count = Checkin.objects.filter(lesson__in=week_start_lessons).filter(
+        Q(status=CHECKIN_STATUS_EARLY) | Q(status=CHECKIN_STATUS_LATEEARLY)).count()
+    course_normal_count = Checkin.objects.filter(lesson__in=week_start_lessons).filter(
+        status=CHECKIN_STATUS_EARLY).count()
+    data.update({'student_all_arrive': student_all_arrive,
+                 'student_all_normal': student_all_normal,
+                 'student_public_ask': student_public_ask,
+                 'student_private_ask': student_private_ask,
+                 'student_sick_ask': student_sick_ask,
+                 'student_other': student_other,
+                 'student_late_count': student_late_count,
+                 'student_early_count': student_early_count,
+                 'course_late_count': course_late_count,
+                 'course_early_count': course_early_count,
+                 'course_normal_count': course_normal_count,
+                 'student_all_count': student_all_count,
+                 'student_normal_count': student_normal_count})
+
+    # Department status
+    department_list = Department.objects.filter(pk__in=Student.objects.distinct('department').values_list('department'))
+    data['department_list'] = [d.name for d in department_list]
+
+    department_normal_value = []
+    department_ask_value = []
+    department_success_value = []
+    department_late_value = []
+    department_early_value = []
+    department_lateearly_value = []
+
+    for d in department_list:
+        res = Checkin.objects.filter(lesson__in=week_start_lessons, student__in=d.student_set.all()).aggregate(
+            normal=Count(Case(When(status=CHECKIN_STATUS_NORMAL, then=1))),
+            ask=Count(Case(When(status__in=[CHECKIN_STATUS_PRIVATE_ASK, CHECKIN_STATUS_PUBLIC_ASK
+                , CHECKIN_STATUS_SICK_ASK], then=1))),
+            success=Count(Case(When(status=CHECKIN_STATUS_SUCCESS, then=1))),
+            early=Count(Case(When(status=CHECKIN_STATUS_EARLY, then=1))),
+            late=Count(Case(When(status=CHECKIN_STATUS_LATE, then=1))),
+            lateearly=Count(Case(When(status=CHECKIN_STATUS_LATEEARLY, then=1))),
+        )
+        department_normal_value.append(res['normal'])
+        department_ask_value.append(res['ask'])
+        department_success_value.append(res['success'])
+        department_late_value.append(res['late'])
+        department_early_value.append(res['early'])
+        department_lateearly_value.append(res['lateearly'])
+
+    data.update({'department_normal_value': department_normal_value,
+                 'department_ask_value': department_ask_value,
+                 'department_success_value': department_success_value,
+                 'department_late_value': department_late_value,
+                 'department_early_value': department_early_value,
+                 'department_lateearly_value': department_lateearly_value})
+
+    # week time status
+
+    weektime_normal_value = []
+    weektime_ask_value = []
+    weektime_success_value = []
+    weektime_late_value = []
+    weektime_early_value = []
+    weektime_lateearly_value = []
+    weektime_arrive_value = []
+    for day in range(0, 7):
+        res = Checkin.objects.filter(lesson__in=week_start_lessons, lesson__day=day).aggregate(
+            normal=Count(Case(When(status=CHECKIN_STATUS_NORMAL, then=1))),
+            ask=Count(Case(When(status__in=[CHECKIN_STATUS_PRIVATE_ASK, CHECKIN_STATUS_PUBLIC_ASK,
+                                            CHECKIN_STATUS_SICK_ASK], then=1))),
+            success=Count(Case(When(status=CHECKIN_STATUS_SUCCESS, then=1))),
+            early=Count(Case(When(status=CHECKIN_STATUS_EARLY, then=1))),
+            late=Count(Case(When(status=CHECKIN_STATUS_LATE, then=1))),
+            lateearly=Count(Case(When(status=CHECKIN_STATUS_LATEEARLY, then=1))),
+        )
+        weektime_normal_value.append(res['normal'])
+        weektime_ask_value.append(res['ask'])
+        weektime_success_value.append(res['success'])
+        weektime_late_value.append(res['late'])
+        weektime_early_value.append(res['early'])
+        weektime_lateearly_value.append(res['lateearly'])
+        weektime_arrive_value.append(res['lateearly'] + res['early'] + res['late'] + res['success'])
+    data.update({'weektime_normal_value': weektime_normal_value,
+                 'weektime_ask_value': weektime_ask_value,
+                 'weektime_success_value': weektime_success_value,
+                 'weektime_late_value': weektime_late_value,
+                 'weektime_early_value': weektime_early_value,
+                 'weektime_lateearly_value': weektime_lateearly_value,
+                 'weektime_arrive_value': weektime_arrive_value})
 
     return HttpResponse(json.dumps(data), content_type="application/json")
 
--- checkin/urls.py
+++ checkin/urls.py
@@ -30,6 +30,8 @@ urlpatterns = [
     url(r'^dashboard/overview', dashboard.overview, name='dashboard_overview'),
     url(r'^dashboard/today$', dashboard.today, name='dashboard_today'),
     url(r'^dashboard/today_data', dashboard.today_data, name='dashboard_today_data'),
+    url(r'^dashboard/week$', dashboard.week, name='dashboard_week'),
+    url(r'^dashboard/week_data', dashboard.week_data, name='dashboard_week_data'),
     url(r'^dashboard/lesson_data', dashboard.lesson_data, name='dashboard_lesson_data'),
 
     url(r'^checkin_success_test', views.checkin_success_test, name='checkin_success_test'),

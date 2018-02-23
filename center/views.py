d426bcd3cf762583fc851a3dee57d531c87ae98b
xiaohuanshu <xiaohuanshu@gmail.com>
2018-02-23 21:53:17 +0800

    教师首页显示未判作业数量

--- center/views.py
+++ center/views.py
@@ -12,24 +12,22 @@ def home(request):
         teacher = Teacher.objects.get(user=request.user)
         termcourse = teacher.course_set.filter(schoolterm=getCurrentSchoolYearTerm()['term'])
         uncommithomework = None
+        unscorehomework = Homeworkcommit.objects.filter(coursehomework__course__in=termcourse, score=None).count()
     else:
         student = Student.objects.get(user=request.user)
-        # alreadycount = Checkin.objects.filter(studentid=student)
-        # alreadycount.query.group_by = ['lessonruntimeid__lessonid__id']
-
         termcourse = Studentcourse.objects.filter(student=student, course__schoolterm=getCurrentSchoolYearTerm()[
             'term']).values_list('course', flat=True)
         termcourse = Course.objects.filter(id__in=termcourse)
         commithomework = Homeworkcommit.objects.filter(student=student).values_list('coursehomework', flat=True)
         uncommithomework = Coursehomework.objects.filter(course__in=termcourse).exclude(
             id__in=commithomework).select_related('course').all()
-
+        unscorehomework = None
     nowlessontime = getnowlessontime()
     lesson = Lesson.objects.filter(course__in=termcourse, week=nowlessontime['week']).order_by('week', 'day', 'time')
-
     return render(request, 'home.html',
                   {'term': getCurrentSchoolYearTerm(),
                    'uncommithomework': uncommithomework,
+                   'unscorehomework': unscorehomework,
                    'termcourse': termcourse.all(),
                    'weeklesson': lesson.select_related('course').select_related('classroom').all(),
                    })

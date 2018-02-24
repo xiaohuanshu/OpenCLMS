46c3167b08e2f3b9bd6a79e6add02a9b79a7556d
xiaohuanshu <xiaohuanshu@gmail.com>
2018-02-24 18:24:57 +0800

    milestone:项目更名OpenCLMS

--- school/context_processors.py
+++ school/context_processors.py
@@ -2,4 +2,4 @@ from django.conf import settings
 
 
 def school_settings(request):
-    return {'WEEK_FIRST_DAY': settings.WEEK_FIRST_DAY}
+    return {'WEEK_FIRST_DAY': settings.WEEK_FIRST_DAY, 'SCHOOL_NAME': settings.SCHOOL_NAME}

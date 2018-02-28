3d55ab3bc5872c9b69979d0a287e37a9ca986a21
xiaohuanshu <xiaohuanshu@gmail.com>
2018-02-28 14:52:44 +0800

    修改bypy自动备份参数

--- docker/auto-backup.sh
+++ docker/auto-backup.sh
@@ -8,5 +8,7 @@ docker exec -t checkinsystem_postgres_1 rm -rf /tmp/dump_$BACKUP_DATE.dmp
 find . -mtime +6 -name "*.dmp" -exec rm -rf {} \;
 
 cd /var/lib/docker/volumes/web_media/_data
-/usr/local/bin/bypy upload --chunk 1T --rapid-upload-only
+/usr/local/bin/bypy -s 1T upload homeworkfile homeworkfile
+/usr/local/bin/bypy -s 1T upload courseresource courseresource
+/usr/local/bin/bypy -s 1T upload daily daily
 exit
